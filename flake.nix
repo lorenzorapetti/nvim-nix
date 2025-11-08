e0924fe04c2ba4a5ccb1d53a11ce3a4b88494f67
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-11-08 22:13:57 +0100

    feat: first commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    mnw.url = "github:Gerg-L/mnw";
+  };
+  outputs = {
+    nixpkgs,
+    mnw,
+    self,
+    ...
+  }: {
+    packages.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      default = mnw.lib.wrap pkgs {
+        neovim = pkgs.neovim-unwrapped;
+
+        # all files in the `lua/lazy` folder are now autoloaded, so no need
+        # for an init.lua in there
+        initLua = ''
+          require('nvim')
+          require('lz.n').load('plugins')
+        '';
+        plugins = {
+          start = [
+            pkgs.vimPlugins.lz-n
+            pkgs.vimPlugins.plenary-nvim
+          ];
+
+          # Anything that you're loading lazily should be put here
+          opt = [
+            pkgs.vimPlugins.telescope-nvim
+          ];
+
+          dev.myconfig = {
+            # you can use lib.fileset to reduce rebuilds here
+            # https://noogle.dev/f/lib/fileset/toSource
+            pure = ./.;
+            impure =
+              # This is a hack it should be a absolute path
+              # here it'll only work from this directory
+              "/' .. vim.uv.cwd()  .. '/nvim";
+          };
+        };
+      };
+
+      dev = self.packages.x86_64-linux.default.devMode;
+    };
+  };
+}
