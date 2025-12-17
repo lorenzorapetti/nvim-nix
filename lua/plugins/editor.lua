e0a8fd4b0fdb12bdf988b071489d05d7ec9ca4d6
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-12-17 22:48:59 +0100

    feat: update mini.files keymaps and layout

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -95,57 +95,57 @@ return {
         end,
       })
 
+      -- Window width based on the offset from the center, i.e. center window
+      -- is 60, then next over is 20, then the rest are 10.
+      -- Can use more resolution if you want like { 60, 20, 20, 10, 5 }
+      local widths = { 60, 20, 10 }
+
       vim.api.nvim_create_autocmd('User', {
         pattern = 'MiniFilesWindowUpdate',
         callback = function(ev)
-          local state = require('mini.files').get_explorer_state() or {}
-
-          local win_ids = vim.tbl_map(function(t)
-            return t.win_id
-          end, state.windows or {})
-
-          local function idx(win_id)
-            for i, id in ipairs(win_ids) do
-              if id == win_id then
-                return i
-              end
-            end
+          local state = MiniFiles.get_explorer_state()
+          if state == nil then
+            return
           end
 
-          local this_win_idx = idx(ev.data.win_id)
-          local focused_win_idx = idx(vim.api.nvim_get_current_win())
-
-          -- this_win_idx can be nil sometimes when opening fresh minifiles
-          if this_win_idx and focused_win_idx then
-            -- idx_offset is 0 for the currently focused window
-            local idx_offset = this_win_idx - focused_win_idx
-
-            -- the width of windows based on their distance from the center
-            -- i.e. center window is 60, then next over is 20, then the rest are 10.
-            -- Can use more resolution if you want like { 60, 30, 20, 15, 10, 5 }
-            local widths = { 60, 20, 10 }
-
-            local i = math.abs(idx_offset) + 1 -- because lua is 1-based lol
-            local win_config = vim.api.nvim_win_get_config(ev.data.win_id)
-            win_config.width = i <= #widths and widths[i] or widths[#widths]
-
-            local offset = 0
-            for j = 1, math.abs(idx_offset) do
-              local w = widths[j] or widths[#widths]
-              -- and an extra +2 each step to account for the border width
-              local _offset = 0.5 * (w + win_config.width) + 2
-              if idx_offset > 0 then
-                offset = offset + _offset
-              elseif idx_offset < 0 then
-                offset = offset - _offset
-              end
+          -- Compute "depth offset" - how many windows are between this and focused
+          local path_this = vim.api.nvim_buf_get_name(ev.data.buf_id):match '^minifiles://%d+/(.*)$'
+          local depth_this
+          for i, path in ipairs(state.branch) do
+            if path == path_this then
+              depth_this = i
             end
-
-            win_config.height = idx_offset == 0 and 25 or 20
-            win_config.row = math.floor(0.5 * (vim.o.lines - win_config.height))
-            win_config.col = math.floor(0.5 * (vim.o.columns - win_config.width) + offset)
-            vim.api.nvim_win_set_config(ev.data.win_id, win_config)
           end
+          if depth_this == nil then
+            return
+          end
+          local depth_offset = depth_this - state.depth_focus
+
+          -- Adjust config of this event's window
+          local i = math.abs(depth_offset) + 1
+          local win_config = vim.api.nvim_win_get_config(ev.data.win_id)
+          win_config.width = i <= #widths and widths[i] or widths[#widths]
+
+          win_config.zindex = 99
+          win_config.col = math.floor(0.5 * (vim.o.columns - widths[1]))
+          local sign = depth_offset == 0 and 0 or (depth_offset > 0 and 1 or -1)
+          for j = 1, math.abs(depth_offset) do
+            -- widths[j+1] for the negative case because we don't want to add the center window's width
+            local prev_win_width = (sign == -1 and widths[j + 1]) or widths[j] or widths[#widths]
+            -- Add an extra +2 each step to account for the border width
+            local new_col = win_config.col + sign * (prev_win_width + 2)
+            if (new_col < 0) or (new_col + win_config.width > vim.o.columns) then
+              win_config.zindex = win_config.zindex - 1
+              break
+            end
+            win_config.col = new_col
+          end
+
+          win_config.height = depth_offset == 0 and 24 or 20
+          win_config.row = math.floor(0.5 * (vim.o.lines - win_config.height))
+          -- win_config.border = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" }
+          win_config.footer = { { tostring(depth_offset), 'Normal' } }
+          vim.api.nvim_win_set_config(ev.data.win_id, win_config)
         end,
       })
     end,
@@ -156,6 +156,13 @@ return {
           require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
         end,
         desc = 'Open mini.files (Directory of Current File)',
+      },
+      {
+        '<leader>fm',
+        function()
+          require('mini.files').open(Util.root(), true)
+        end,
+        desc = 'Open mini.files (Root)',
       },
       {
         '<leader>fM',
