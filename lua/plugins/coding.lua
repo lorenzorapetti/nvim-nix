3d6de5859a3143b50adf09462a0dad6cf7d3b380
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-12-17 22:19:26 +0100

    feat: add textobjects to mini.ai

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -58,6 +58,53 @@ return {
           f = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
 
           t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' }, -- tags
+
+          d = { '%f[%d]%d+' }, -- digits
+
+          -- snake_case, camelCase, PascalCase, etc; all capitalizations
+          e = {
+            -- Lua 5.1 character classes and the undocumented frontier pattern:
+            -- https://www.lua.org/manual/5.1/manual.html#5.4.1
+            -- http://lua-users.org/wiki/FrontierPattern
+            -- note: when I say "letter" I technically mean "letter or digit"
+            {
+              -- Matches a single uppercase letter followed by 1+ lowercase letters.
+              -- This covers:
+              -- - PascalCaseWords (or the latter part of camelCaseWords)
+              '%u[%l%d]+%f[^%l%d]', -- An uppercase letter, 1+ lowercase letters, to end of lowercase letters
+
+              -- Matches lowercase letters up until not lowercase letter.
+              -- This covers:
+              -- - start of camelCaseWords (just the `camel`)
+              -- - snake_case_words in lowercase
+              -- - regular lowercase words
+              '%f[^%s%p][%l%d]+%f[^%l%d]', -- after whitespace/punctuation, 1+ lowercase letters, to end of lowercase letters
+              '^[%l%d]+%f[^%l%d]', -- after beginning of line, 1+ lowercase letters, to end of lowercase letters
+
+              -- Matches uppercase or lowercase letters up until not letters.
+              -- This covers:
+              -- - SNAKE_CASE_WORDS in uppercase
+              -- - Snake_Case_Words in titlecase
+              -- - regular UPPERCASE words
+              -- (it must be both uppercase and lowercase otherwise it will
+              -- match just the first letter of PascalCaseWords)
+              '%f[^%s%p][%a%d]+%f[^%a%d]', -- after whitespace/punctuation, 1+ letters, to end of letters
+              '^[%a%d]+%f[^%a%d]', -- after beginning of line, 1+ letters, to end of letters
+            },
+            '^().*()$',
+          },
+
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+
+          -- Whole buffer
+          g = function()
+            local from = { line = 1, col = 1 }
+            local to = {
+              line = vim.fn.line '$',
+              col = math.max(vim.fn.getline('$'):len(), 1),
+            }
+            return { from = from, to = to }
+          end,
         },
 
         -- Module mappings. Use `''` (empty string) to disable one.
