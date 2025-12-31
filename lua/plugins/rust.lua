835abc1d84e3c83462c424eb4166ad16ec52e62e
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-12-31 22:46:42 +0100

    fix: enable diagnostics for rustaceanvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -27,10 +27,10 @@ return {
                 },
               },
               -- Add clippy lints for Rust if using rust-analyzer
-              checkOnSave = diagnostics == 'rust-analyzer',
+              checkOnSave = true,
               -- Enable diagnostics if using rust-analyzer
               diagnostics = {
-                enable = diagnostics == 'rust-analyzer',
+                enable = true,
               },
               procMacro = {
                 enable = true,
