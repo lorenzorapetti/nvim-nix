c0228731d43568af7c2e62c9ca4b45058e47dcea
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-02-07 21:48:27 +0100

    feat: add rustaceanvim keymaps

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -7,15 +7,44 @@ return {
       vim.g.rustaceanvim = {
         server = {
           on_attach = function(_, bufnr)
-            vim.keymap.set('n', '<leader>ca', function()
+            local function map(mode, lhs, rhs, desc)
+              vim.keymap.set(mode, lhs, rhs, { desc = desc, silent = true, buffer = bufnr })
+            end
+
+            map('n', '<leader>ca', function()
               vim.cmd.RustLsp 'codeAction'
-            end, { desc = 'Code Action', silent = true, buffer = bufnr })
-            vim.keymap.set('n', '<leader>dr', function()
-              vim.cmd.RustLsp 'debuggables'
-            end, { desc = 'Rust Debuggables', silent = true, buffer = bufnr })
-            vim.keymap.set('n', 'K', function()
+            end, 'Code Action')
+            map('n', '<leader>cm', function()
+              vim.cmd.RustLsp 'expandMacro'
+            end, 'Expand Macro')
+            map('n', '<leader>ck', '<Plug>RustHoverAction', 'Hover Action')
+            map('n', '<leader>ce', function()
+              vim.cmd.RustLsp 'explainError'
+            end, 'Explain Error')
+            map('n', '<leader>cD', function()
+              vim.cmd.RustLsp 'renderDiagnostic'
+            end, 'Rust Diagnostic')
+
+            map('n', '<leader>od', function()
+              vim.cmd.RustLsp 'openDocs'
+            end, 'Open docs.rs documentation')
+            map('n', '<leader>oc', function()
+              vim.cmd.RustLsp 'openCargo'
+            end, 'Open Cargo.toml')
+            map('n', '<leader>ot', function()
+              vim.cmd.RustLsp 'relatedTests'
+            end, 'Open related tests')
+
+            map('n', 'J', function()
+              vim.cmd.RustLsp 'joinLines'
+            end, 'Join Lines')
+            -- Required nvim-dap
+            -- map('n', '<leader>dr', function()
+            --   vim.cmd.RustLsp 'debuggables'
+            -- end, 'Rust Debuggables')
+            map('n', 'K', function()
               vim.cmd.RustLsp { 'hover', 'actions' }
-            end, { desc = 'Hover Information', silent = true, buffer = bufnr })
+            end, 'Hover Information')
           end,
           load_vscode_settings = false,
           -- the global hook doesn't work when configuring rust-analyzer with rustaceanvim
